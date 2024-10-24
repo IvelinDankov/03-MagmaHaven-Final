47fbd89be14bbfd39a96a69048b905f491719933
Velin Dankov <velin80@web.de>
2024-10-24 14:12:23 +0300

    edit done!

--- src/controllers/volcanoController.js
+++ src/controllers/volcanoController.js
@@ -7,24 +7,22 @@ const router = Router();
 /* ############################
 ########### CATALOG
 ############################# */
-router.get('/catalog', async (req, res) => {
-   const volcanoes = await volcanoService.getAll().lean();
-   res.render("volcano/catalog", { title: "Catalog Page", volcanoes });
-})
-
-
+router.get("/catalog", async (req, res) => {
+  const volcanoes = await volcanoService.getAll().lean();
+  res.render("volcano/catalog", { title: "Catalog Page", volcanoes });
+});
 
 /* ############################
 ########### CREATE
 ############################# */
 
 router.get("/create", (req, res) => {
-
-   const volcanoDataType = getVolcanoTypeViewData({});
-  res.render("volcano/create", { title: "Edit Page", volcanoDataType });
+  const volcanoDataType = getVolcanoTypeViewData({});
+  res.render("volcano/create", {
+    title: "Edit Page",
+    volcanoTypes: volcanoDataType,
+  });
 });
-
-
 
 router.post("/create", async (req, res) => {
   const volcanoData = req.body;
@@ -50,14 +48,85 @@ router.post("/create", async (req, res) => {
 ########### DETAILs
 ############################# */
 
-router.get('/:volcanoId/details',async (req, res) => {
-   const volcanoId = req.params.volcanoId;
+router.get("/:volcanoId/details", async (req, res) => {
+  const volcanoId = req.params.volcanoId;
 
-   const volcano = await volcanoService.getOne(volcanoId).lean();
-   res.render('volcano/details', {title: "Details Page", volcano});
+  const volcano = await volcanoService.getOne(volcanoId).lean();
+
+  const isVoted = volcano.voteList.some((userId) => userId == req.user?._id);
+  const isOwner = volcano.owner.toString() === req.user?._id;
+  res.render("volcano/details", {
+    title: "Details Page",
+    volcano,
+    isOwner,
+    isVoted,
+  });
 });
 
-// FIXME: This is function for options and. 
+/* ############################
+########### VOTE
+############################# */
+
+router.get("/:volcanoId/vote", async (req, res) => {
+  const volcanoId = req.params.volcanoId;
+  const userId = req.user._id;
+
+  try {
+    await volcanoService.vote(volcanoId, userId);
+
+    return res.redirect(`/volcano/${volcanoId}/details`);
+  } catch (err) {
+    // TODO: Error
+    console.log(err.message);
+  }
+});
+
+/* ############################
+########### DELETE
+############################# */
+
+router.get("/:volcanoId/delete", async (req, res) => {
+  const volcanoId = req.params.volcanoId;
+  await volcanoService.remove(volcanoId);
+
+  res.redirect("/volcano/catalog");
+});
+
+/* ############################
+########### EDIT
+############################# */
+
+router.get("/:volcanoId/edit", async (req, res) => {
+  const volcanoId = req.params.volcanoId;
+  const volcano = await volcanoService.getOne(volcanoId).lean();
+  const volcanoDataType = getVolcanoTypeViewData(volcano);
+
+  res.render("volcano/edit", {
+    title: "Edit Page",
+    volcano,
+    volcanoTypes: volcanoDataType,
+  });
+});
+
+router.post("/:volcanoId/edit", async (req, res) => {
+  const volcanoId = req.params.volcanoId;
+  const volcanoData = req.body;
+
+  try {
+    await volcanoService.edit(volcanoId, volcanoData);
+
+    res.redirect(`/volcano/${volcanoId}/details`);
+  } catch (err) {
+    const error = getErrorMsg(err);
+    res.render("volcano/edit", {
+      title: "Edit Page",
+      volcano: volcanoData,
+      error,
+    });
+  }
+});
+
+// FIXME: This is function for options and.
 function getVolcanoTypeViewData({ typeVolcano }) {
   const volcanoTypes = [
     "Supervolcanoes",
--- src/services/volcanoService.js
+++ src/services/volcanoService.js
@@ -12,8 +12,23 @@ const getOne = (id) => {
   return Volcano.findById(id);
 };
 
+const vote = (volcanoId, userId) => {
+  return Volcano.findByIdAndUpdate( volcanoId, {$push: { voteList: userId } });
+};
+
+const remove = (volcanoId) => {
+  return Volcano.findByIdAndDelete(volcanoId);
+}
+
+const edit = (volcanoId, data) => {
+  return Volcano.findByIdAndUpdate(volcanoId, data);
+}
+
 export default {
   getAll,
   create,
   getOne,
+  vote,
+  remove,
+  edit
 };
